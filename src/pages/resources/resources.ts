d8631a0add743e8c4c15f56fc0ddab2b39c0916d
Evelyn Caballero <eacabal1@uci.edu>
2018-03-08 21:51:48 -0800

    added more to the app

--- src/pages/resources/resources.ts
+++ src/pages/resources/resources.ts
@@ -6,9 +6,27 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'resources.html'
 })
 export class ResourcesPage {
+  shownGroup = null;
 
+  diseases = [
+      { title: "National Hotline for Street Harrasment", description: "Call toll-free:<font color='#FF334C'> 855-897-5910</font><br><br><font color='#FF334C' size= 2> Visit the online hotline </font>\
+      <font size=2>to receive help over secure, anonymous IM chat. It is compatible with smartphones, tablets, computers, and other internet-enabled devices (English | Spanish) <br><br> Help is available 24/7.</font>"},
+      { title: "Dealing with Harrasers", description: "<font size= 2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</font>" },
+      { title: "What to do Before or After Harrasment", description: "<font size= 2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</font>" },
+      { title: "Street Harrasment and the Law", description: "<font size= 2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</font>" },
+      { title: "Anti-Harrasment Groups and Campaigns", description: "<font size= 2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</font>" }
+    ];
   constructor(public navCtrl: NavController) {
 
   }
-
+  toggleGroup(group) {
+    if (this.isGroupShown(group)) {
+        this.shownGroup = null;
+    } else {
+        this.shownGroup = group;
+    }
+};
+  isGroupShown(group) {
+    return this.shownGroup === group;
+};
 }
